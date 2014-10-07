f23aa3fcef94b900447242869e697960ff122636
App Academy Student <student@appacademy.io>
2014-10-07 18:04:29 -0400

    "Finish through phase 5"

--- skeleton/lib/phase4/session.rb
+++ skeleton/lib/phase4/session.rb
@@ -6,17 +6,30 @@ module Phase4
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      @req = req
+      cookies = @req.cookies.select { |cookie| cookie.name == "_rails_lite_app" }
+
+      if cookies.empty?
+        @cookie_value = {}
+      else
+        cookie = cookies.first
+        @cookie_value = JSON.parse(cookie.value)
+      end
     end
 
     def [](key)
+      @cookie_value[key]
     end
 
     def []=(key, val)
+      @cookie_value[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      value = @cookie_value.to_json
+      res.cookies << WEBrick::Cookie.new("_rails_lite_app", value)
     end
   end
 end
--- skeleton/lib/phase5/params.rb
+++ skeleton/lib/phase5/params.rb
@@ -7,9 +7,17 @@ module Phase5
     # 2. post body
     # 3. route params
     def initialize(req, route_params = {})
+      @req = req
+      @params = route_params
+      if @req.query_string
+        parse_www_encoded_form(@req.query_string)
+      elsif @req.body
+        parse_www_encoded_form(@req.body)
+      end
     end
 
     def [](key)
+      @params[key.to_s]
     end
 
     def to_s
@@ -25,11 +33,47 @@ module Phase5
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      param_array = URI::decode_www_form(www_encoded_form)
+
+      param_array = param_array.map do |param|
+          [parse_key(param[0]), param[1]]
+      end
+
+      param_array.each do |param|
+        if param[0].is_a?(Array)
+          hashify_param(param)
+        else
+          @params[param[0]] = param[1]
+        end
+      end
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      if key.include?("]")
+        key = key.split(/\]\[|\]|\[/)
+      else
+        key
+      end
+
+      key
     end
+
+    def hashify_param(param)
+      scope = @params
+
+      param[0].each_with_index do |key, index|
+        if index == param[0].count - 1
+          scope[key] = param[1]
+        else
+          scope[key] ||= {}
+          scope = scope[key]
+        end
+      end
+    end
+
   end
 end
+
+
